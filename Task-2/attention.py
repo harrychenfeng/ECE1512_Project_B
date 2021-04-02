2109c4e91734116b43110663010a15de92ec93ed
harrychenfeng <harrychenfeng@gmail.com>
2021-04-02 15:18:52 +0800

    add task2

---
+++ Task-2/attention.py
@@ -0,0 +1,256 @@
+# Reference: https://github.com/jacobgil/pytorch-grad-cam 
+# Note: Change the implementation of Grad-CAM from above link to the attention-based method proposed by 
+# the paper, https://bmvc2019.org/wp-content/uploads/papers/0491-paper.pdf 
+import torch
+import argparse
+import cv2
+import numpy as np
+import torch
+from torch.autograd import Function
+from torchvision import models, transforms
+from train import *
+from utils import *
+
+class FeatureExtractor():
+    """ Class for extracting activations and
+    registering gradients from targetted intermediate layers """
+
+    def __init__(self, model, target_layers):
+        self.model = model
+        self.target_layers = target_layers
+        self.gradients = []
+
+    def save_gradient(self, grad):
+        self.gradients.append(grad)
+
+    def __call__(self, x):
+        outputs = []
+        self.gradients = []
+        for name, module in self.model._modules.items():
+            x = module.forward(x)
+            if name in self.target_layers:
+                x.register_hook(self.save_gradient)
+                outputs += [x]
+        return outputs, x
+
+class ModelOutputs():
+    """ Class for making a forward pass, and getting:
+    1. The network output.
+    2. Activations from intermeddiate targetted layers.
+    3. Gradients from intermeddiate targetted layers. """
+
+    def __init__(self, model, feature_module, target_layers):
+        self.model = model
+        self.feature_module = feature_module
+        self.feature_extractor = FeatureExtractor(self.feature_module, target_layers)
+
+    def get_gradients(self):
+        return self.feature_extractor.gradients
+
+    def __call__(self, x):
+        target_activations = []
+        for name, module in self.model._modules.items():
+            if module == self.feature_module:
+                target_activations, x = self.feature_extractor(x)
+            elif "avgpool" in name.lower():
+                x = module(x)
+                x = x.view(x.size(0),-1)
+            else:
+                x = module.forward(x)
+
+        return target_activations, x
+
+def preprocess_image(img):
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+    preprocessing = transforms.Compose([
+        transforms.ToTensor(),
+        normalize,
+    ])
+    return preprocessing(img.copy()).unsqueeze(0)
+
+def show_cam_on_image(img, mask):
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    cam = heatmap + np.float32(img)
+    cam = cam / np.max(cam)
+    return np.uint8(255 * cam)
+
+class Attention:
+    def __init__(self, model, feature_module, target_layer_names, use_cuda):
+        self.model = model
+        self.feature_module = feature_module
+        self.model.eval()
+        self.cuda = use_cuda
+        if self.cuda:
+            self.model = model.cuda()
+
+        self.extractor = ModelOutputs(self.model, self.feature_module, target_layer_names)
+
+    def forward(self, input_img):
+        return self.model(input_img)
+
+    def __call__(self, input_img, target_category=None):
+        if self.cuda:
+            input_img = input_img.cuda()
+
+        features, output = self.extractor(input_img)
+        
+        # Generate one-hot-encoding based on the difference of 
+        # the heatmap produced by the ResNet and the true heat map
+        heatmap = self.model.forward(input_img).squeeze(0)
+        one_hot = target_category.requires_grad_(True)
+        one_hot = torch.sum(one_hot * output)
+
+        self.feature_module.zero_grad()
+        self.model.zero_grad()
+        one_hot.backward(retain_graph=True)
+
+        grads_val = self.extractor.get_gradients()[-1].cpu().data.numpy()
+
+        target = features[-1]
+        target = target.cpu().data.numpy()[0, :]
+        
+        # Implement harmonic attention map
+        # Paper link: https://bmvc2019.org/wp-content/uploads/papers/0491-paper.pdf
+        channel_att = np.zeros(target.shape[1:], dtype=np.float32)
+        positive_weights = np.mean(np.maximum(grads_val, 0), axis=(2, 3))[0, :]
+        pixel_att = np.multiply(grads_val[0], target)
+        
+        for k, w in enumerate(positive_weights):
+            channel_att += w * pixel_att[k,:,:]
+        
+        harmonic_att = np.maximum(channel_att, 0)
+        harmonic_att = cv2.resize(harmonic_att, input_img.shape[2:])
+        harmonic_att = harmonic_att - np.min(harmonic_att)
+        harmonic_att = harmonic_att / np.max(harmonic_att)
+        
+        return harmonic_att
+
+
+class GuidedBackpropReLU(Function):
+    @staticmethod
+    def forward(self, input_img):
+        positive_mask = (input_img > 0).type_as(input_img)
+        output = torch.addcmul(torch.zeros(input_img.size()).type_as(input_img), input_img, positive_mask)
+        self.save_for_backward(input_img, output)
+        return output
+
+    @staticmethod
+    def backward(self, grad_output):
+        input_img, output = self.saved_tensors
+        grad_input = None
+
+        positive_mask_1 = (input_img > 0).type_as(grad_output)
+        positive_mask_2 = (grad_output > 0).type_as(grad_output)
+        grad_input = torch.addcmul(torch.zeros(input_img.size()).type_as(input_img),
+                                   torch.addcmul(torch.zeros(input_img.size()).type_as(input_img), grad_output,
+                                                 positive_mask_1), positive_mask_2)
+        return grad_input
+
+
+class GuidedBackpropReLUModel:
+    def __init__(self, model, use_cuda):
+        self.model = model
+        self.model.eval()
+        self.cuda = use_cuda
+        if self.cuda:
+            self.model = model.cuda()
+
+        def recursive_relu_apply(module_top):
+            for idx, module in module_top._modules.items():
+                recursive_relu_apply(module)
+                if module.__class__.__name__ == 'ReLU':
+                    module_top._modules[idx] = GuidedBackpropReLU.apply
+
+        # replace ReLU with GuidedBackpropReLU
+        recursive_relu_apply(self.model)
+
+    def forward(self, input_img):
+        return self.model(input_img)
+
+    def __call__(self, input_img, target_category=None):
+        if self.cuda:
+            input_img = input_img.cuda()
+
+        input_img = input_img.requires_grad_(True)
+
+        output = self.forward(input_img)
+
+        one_hot = target_category.requires_grad_(True)
+
+        one_hot = torch.sum(one_hot * output)
+        one_hot.backward(retain_graph=True)
+
+        output = input_img.grad.cpu().data.numpy()
+        output = output[0, :, :, :]
+
+        return output
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use-cuda', action='store_true', default=False,
+                        help='Use NVIDIA GPU acceleration')
+    parser.add_argument('--image-path', type=str, default='./examples/both.png',
+                        help='Input image path')
+    args = parser.parse_args()
+    args.use_cuda = args.use_cuda and torch.cuda.is_available()
+    if args.use_cuda:
+        print("Using GPU for acceleration")
+    else:
+        print("Using CPU for computation")
+
+    return args
+
+def deprocess_image(img):
+    """ see https://github.com/jacobgil/keras-grad-cam/blob/master/grad-cam.py#L65 """
+    img = img - np.mean(img)
+    img = img / (np.std(img) + 1e-5)
+    img = img * 0.1
+    img = img + 0.5
+    img = np.clip(img, 0, 1)
+    return np.uint8(img*255)
+
+if __name__ == '__main__':
+    """ python grad_cam.py <path_to_image>
+    1. Loads an image with opencv.
+    2. Preprocesses it for ResNet50 and converts to a pytorch variable.
+    3. Makes a forward pass to find the category index with the highest score,
+    and computes intermediate activations.
+    Makes the visualization. """
+
+    args = get_args()
+
+    # Using trained tuned_model 
+    model = loadnet(10, './model/tuned_model.pth')
+    attention = Attention(model=model, feature_module=model.layer3, \
+                       target_layer_names=["5"], use_cuda=args.use_cuda)
+
+    img = cv2.imread(args.image_path, 1)
+    img = np.float32(img) / 255
+    # Opencv loads as BGR:
+    img = img[:, :, ::-1]
+    input_img = preprocess_image(img)
+    
+    # Since this ResNet does not have a logistic layer for classification
+    # The true label for it would be a 'true map' generated by calling
+    # the provided 'get_true_map' function from train.py
+    heatmap = model.forward(input_img).squeeze(0)
+    target_category = get_true_map(heatmap)
+
+    grayscale_cam = attention(input_img, target_category)
+
+    grayscale_cam = cv2.resize(grayscale_cam, (img.shape[1], img.shape[0]))
+    att = show_cam_on_image(img, grayscale_cam)
+
+    gb_model = GuidedBackpropReLUModel(model=model, use_cuda=args.use_cuda)
+    gb = gb_model(input_img, target_category=target_category)
+    gb = gb.transpose((1, 2, 0))
+
+    att_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
+    att_gb = deprocess_image(att_mask*gb)
+    gb = deprocess_image(gb)
+
+    cv2.imwrite("attention.jpg", att)
+    cv2.imwrite('gb.jpg', gb)
+    cv2.imwrite('attention_gb.jpg', att_gb)
